ed276797af2ddfad0ca22884e4771ab512f45e99
Radek Mocek <dadun001@email.cz>
2026-09-21 16:26:02 +0200

    Feat: Database backup/restore to/from a file (#1)

--- lib/db.dart
+++ lib/db.dart
@@ -1,3 +1,6 @@
+import "dart:io";
+import "dart:typed_data";
+
 import "package:my_beer_diary/data.dart";
 import "package:my_beer_diary/model/beer.dart";
 import "package:my_beer_diary/model/beer_consumption.dart";
@@ -14,6 +17,15 @@ class AppDatabase {
   static final _databaseVersion = 6;
   static Database? _database;
 
+  Future<String> get _dbPath async =>
+      join(await getDatabasesPath(), _databaseName);
+
+  Future<void> close() async {
+    await _database?.close();
+    _database = null;
+    // Will lazily reopen
+  }
+
   Future<Database> get database async {
     if (_database != null) {
       return _database!;
@@ -23,10 +35,8 @@ class AppDatabase {
   }
 
   Future<Database> _initDatabase() async {
-    final defaultPath = await getDatabasesPath();
-    final dbPath = join(defaultPath, _databaseName);
     return await openDatabase(
-      dbPath,
+      await _dbPath,
       version: _databaseVersion,
       onCreate: _onCreate,
       onUpgrade: _onUpgrade,
@@ -52,5 +62,58 @@ class AppDatabase {
     await db.execute(beerConsumptionTableDrop);
 
     await _onCreate(db, newVersion);
+  }
+
+  // .: Database backup :.
+  // .:=================:.
+
+  Future<Uint8List> exportBytes() async {
+    await close();
+    return File(await _dbPath).readAsBytes();
+  }
+
+  /// Restores data from backup database on [path] – this deletes all local data (current database).
+  /// Returns a [bool] indicating whether the restore was successful
+  /// and [String] with error message if it wasn't.
+  Future<(bool, String)> restore(String path) async {
+    final source = File(path);
+
+    // Copy the new database file to out database directory,
+    // do not overwrite the original database yet, we'll check this db first
+    final dbDir = await getDatabasesPath();
+    final tempPath = join(dbDir, "temporary.db");
+    await source.copy(tempPath);
+
+    // We'll check if the database has the required tables for the app to work
+    // This should prevent accidental loading of some database that is not from this app
+    const expectedTableNames = [
+      tagTable,
+      eventTable,
+      beerTable,
+      beerConsumptionTable,
+    ];
+
+    try {
+      final tempDb = await openDatabase(tempPath, readOnly: true);
+
+      // Check the database for expected tables
+      final tableSet = (await tempDb.rawQuery(
+        "SELECT name FROM sqlite_master WHERE type='table'",
+      )).map((row) => row["name"]).toSet();
+
+      for (final expectedTableName in expectedTableNames) {
+        if (!tableSet.contains(expectedTableName)) {
+          return (false, "V databázi chybí tabulka „$expectedTableName“.");
+        }
+      }
+
+      // tempDb is OK: close current database and replace it
+      await close();
+      await File(tempPath).copy(await _dbPath);
+    } catch (e) {
+      return (false, e.toString());
+    }
+
+    return (true, "");
   }
 }
--- lib/screen/about_screen.dart
+++ lib/screen/about_screen.dart
@@ -1,5 +1,9 @@
+import "package:file_picker/file_picker.dart";
+import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:my_beer_diary/common.dart";
+import "package:my_beer_diary/db.dart";
+import "package:my_beer_diary/widget/text_divider.dart";
 import "package:url_launcher/url_launcher.dart" show launchUrl;
 
 final Uri githubURL = Uri.parse("https://github.com/RDMCz/MyBeerDiary");
@@ -10,37 +14,134 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("O aplikaci")),
-      body: Padding(
-        padding: CardListCommon.horizontalPaddingOnly,
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            children: [
-              Text(
-                "\nMůj pivní deníček\n",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28.0),
-              ),
-              Image.asset("asset/icon/appicon.png", height: 150),
-              DefaultTextStyle.merge(
-                style: TextStyle(fontSize: 16.0),
-                child: Column(
-                  children: [
-                    Text("\nverze 0.1.0\n"),
-                    InkWell(
-                      onTap: () async => await launchUrl(githubURL),
-                      child: Text(
-                        "github.com/RDMCz/MyBeerDiary",
-                        style: TextStyle(
-                          color: Colors.blueAccent,
-                          decoration: TextDecoration.underline,
-                          decorationColor: Colors.blueAccent,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: CardListCommon.horizontalPaddingOnly,
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Text(
+                  "\nMůj pivní deníček\n",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28.0),
+                ),
+                Image.asset("asset/icon/appicon.png", height: 150),
+                DefaultTextStyle.merge(
+                  style: TextStyle(fontSize: 16.0),
+                  child: Column(
+                    children: [
+                      Text("\nverze 0.1.0\n"),
+                      InkWell(
+                        onTap: () async => await launchUrl(githubURL),
+                        child: Text(
+                          "github.com/RDMCz/MyBeerDiary",
+                          style: TextStyle(
+                            color: Colors.blueAccent,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.blueAccent,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 28),
+                      if (!kIsWeb) ...[
+                        TextDivider(text: "ZÁLOHA DATABÁZE"),
+                        Text(
+                          "Přecházíte-li na nové zařízení, je možné do něj zkopírovat svá pivní data."
+                          "\n1. Na starém zařízení zálohujte databázi do souboru"
+                          "\n2. Soubor si pošlete do nového zařízení"
+                          "\n3. Na novém zařízení obnovte databázi z tohoto souboru",
+                        ),
+                        SizedBox(height: 16),
+                        TextButton.icon(
+                          onPressed: () async {
+                            final dbBytes = await AppDatabase.instance
+                                .exportBytes();
+
+                            await FilePicker.saveFile(
+                              dialogTitle: "Záloha databáze",
+                              fileName: "MujPivniDenicek_Zaloha.db",
+                              bytes: dbBytes,
+                            );
+                          },
+                          label: Text("Zálohovat databázi do souboru"),
+                          icon: Icon(Icons.backup_outlined),
+                        ),
+                        TextButton.icon(
+                          onPressed: () async {
+                            final confirmationResult = await showDialog(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: Text("Obnovit databázi"),
+                                content: Text(
+                                  "Opravdu si přejete obnovit databázi ze souboru?\n\nVšechna lokální data budou smazána!",
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(false),
+                                    child: Text("Zrušit"),
+                                  ),
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: Text("Pokračovat"),
+                                  ),
+                                ],
+                              ),
+                            );
+
+                            if (confirmationResult ?? false) {
+                              final fileResult = await FilePicker.pickFile(
+                                allowedExtensions: ["db"],
+                                dialogTitle: "Obnova databáze",
+                              );
+                              final path = fileResult?.path;
+
+                              if (path != null) {
+                                final (result, message) = await AppDatabase
+                                    .instance
+                                    .restore(path);
+
+                                if (context.mounted) {
+                                  await showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) =>
+                                        AlertDialog(
+                                          title: Text(
+                                            result
+                                                ? "Obnova úspěšná"
+                                                : "Obnova neúspěšná",
+                                          ),
+                                          content: Text(
+                                            result
+                                                ? "Restartujte prosím aplikaci!"
+                                                : message,
+                                          ),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () => Navigator.of(
+                                                context,
+                                              ).pop(true),
+                                              child: Text("OK"),
+                                            ),
+                                          ],
+                                        ),
+                                  );
+                                }
+                              }
+                            }
+                          },
+                          label: Text("Obnovit databázi ze souboru"),
+                          icon: Icon(Icons.restore),
+                        ),
+                      ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                // (Some empty space at the end so the last text isn't near the screen edge)
+                SizedBox(height: 70),
+              ],
+            ),
           ),
         ),
       ),
